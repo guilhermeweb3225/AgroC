a0b74f0c155e655bef25f4c163d6c30c28126036
Guilherme P.Oliveira <guilhermeponciano3225@gmail.com>
2025-10-14 23:53:49 -0300

    visualizaçao de test

--- conect/src/pages/inicio.jsx
+++ conect/src/pages/inicio.jsx
@@ -1,6 +1,9 @@
 
 import "./inicio.css";
 import Vd from "../images/vd3.mp4";
+import Agilidade from "../images/imgagilidade.png";
+import Tecnologia from "../images/imgtecnologia.png";
+import Conexao from "../images/imgconexao.png";
 function Inicio() {
   return (
     <section className="inicio">
@@ -33,17 +36,17 @@ function Inicio() {
         <h2>Por que escolher a AgroConnect?</h2>
         <div className="cards">
           <div className="card">
-            <img src="https://cdn-icons-png.flaticon.com/512/3082/3082026.png" alt="Conexão direta" />
+            <img src={Conexao}alt="Conexão direta" />
             <h3>Conexão direta</h3>
             <p>Produtores e compradores se comunicam sem intermediários, com mais transparência.</p>
           </div>
           <div className="card">
-            <img src="https://cdn-icons-png.flaticon.com/512/4341/4341139.png" alt="Agilidade" />
+            <img src={Agilidade} alt="Agilidade" />
             <h3>Agilidade</h3>
             <p>Negociações rápidas e entregas otimizadas através de nossa rede integrada.</p>
           </div>
           <div className="card">
-            <img src="https://cdn-icons-png.flaticon.com/512/3190/3190571.png" alt="Tecnologia" />
+            <img src={Tecnologia} alt="Tecnologia" />
             <h3>Tecnologia</h3>
             <p>Gestão inteligente e integração digital para simplificar o agronegócio.</p>
           </div>
